42682ce4801a09a89b3efb385918d334bfb42666
Hanshika Ramineni <raminenihansika6@gmail.com>
2026-07-03 18:40:39 +0530

    commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,29 @@
 console.log("Hello from JS");
-alert("Hello from JS");
+// alert("Hello from JS");
+
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "../images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "../images/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  // gender
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
+console.log("Hello from JS");
